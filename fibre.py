4954e9d0b54c105e7064cf9d0c8feb6be38b5447
luisgonzalez4 <luismmmm4@gmail.com>
2026-01-19 21:57:16 -0600

    Add files via upload

--- fibre.py
+++ fibre.py
@@ -1,24 +1,47 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.special import j0
+import pulse as pls
+c = 2.998e8
+def SMF(A,w,beta2,beta3,gamma,L,step):
+     h=L/step
+     D2=0.5j * beta2 * w**2
+     D3=1j/6 * beta3 * w**3
+     D=np.exp((D2+D3)*h)
+     hhz= 1j*gamma*h
+     temp=A*np.exp(np.abs(A**2 )*hhz/2)
+     for i in range(0,step):
+        f_temp=np.fft.ifft(temp)*D
+        A=np.fft.fft(f_temp)
+        temp=A*np.exp(np.abs(A)**2 *hhz)
+     A=temp*np.exp(-np.abs(A)**2 *hhz/2)
+     return A
 
-# Parámetro normalizado típico del modo fundamental (primer cero de J0)
-pa = 2.405  
 
-# Eje normalizado rho/a
-x = np.linspace(0, 3, 500)
+def Coupler(A,alfa):
+    A_ro=np.sqrt(alfa)*A
+    A_bo=1j*np.sqrt(1-alfa)*A
+    return A_ro,A_bo
 
-# Funciones
-J = j0(pa * x)                 # J0(p*rho) con rho = a*x
-w_over_a = 0.65
-G = np.exp(-(x / w_over_a)**2)  # Gaussiana aproximada
 
-# Gráfica
-plt.figure(figsize=(5,4))
-plt.plot(x, J, label=r"$J_0(p\rho)$")
-plt.plot(x, G, label="Gaussiana")
-plt.xlabel(r"$\rho / a$")
-plt.ylabel("Amplitud (normalizada)")
-plt.legend()
-plt.tight_layout()
-plt.show()
+def DopedFibre(A,t,w,lambda0,BW,beta2,gamma,g0,Esat,L,step):
+    h=L/step
+    D2=0.5j * beta2 * w**2
+    Omega_g=-((2*np.pi*c*BW)/(lambda0**2) )
+    GainShape=1/(1+(w/Omega_g)**2)
+    hhz=1j*gamma*h
+    temp=A*np.exp(np.abs(A)**2 * hhz/2)
+    for i in range(step):
+
+        E = pls.Energy(temp, t)
+        g_sat = g0 / (1 + E / Esat)  
+        Linear = np.exp((D2 + 0.5 * g_sat * GainShape) * h)
+
+        f_temp = np.fft.ifft(temp) * Linear
+        A=np.fft.fft(f_temp)
+        temp = A * np.exp(np.abs(A)**2 * hhz)
+    A=temp*np.exp(-np.abs(A)**2 *hhz/2)
+    return A
+
+def SA(A,q0,psat):
+    T=1-(q0 / (1+ (pls.Intensity(A)/psat)) )
+    Aout=T*A
+    return Aout
